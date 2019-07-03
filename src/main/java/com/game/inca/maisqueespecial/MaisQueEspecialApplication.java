e1f31267bd736dc28bb714288d04840084a2dc6d
Ramon do Nascimento Pereira <ramonnascimento@brq.com>
2019-07-03 18:40:57 -0300

    Importação Inicial

---
+++ src/main/java/com/game/inca/maisqueespecial/MaisQueEspecialApplication.java
@@ -0,0 +1,13 @@
+package com.game.inca.maisqueespecial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaisQueEspecialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaisQueEspecialApplication.class, args);
+	}
+
+}
