8647d4d485535c2831a97c9903cf3bf52bb82e7a
Ramon do Nascimento Pereira <ramonnascimento@brq.com>
2019-07-18 19:40:17 -0300

    adicionado serviço de retorno dos jogos por login

--- src/main/java/com/game/inca/maisqueespecial/MaisQueEspecialApplication.java
+++ src/main/java/com/game/inca/maisqueespecial/MaisQueEspecialApplication.java
@@ -2,11 +2,7 @@ package com.game.inca.maisqueespecial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class MaisQueEspecialApplication {
@@ -15,13 +11,5 @@ public class MaisQueEspecialApplication {
 		SpringApplication.run(MaisQueEspecialApplication.class, args);
 		System.out.print(new BCryptPasswordEncoder().encode("123"));
 	}
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-	    return new WebMvcConfigurerAdapter() {
-	        @Override
-	        public void addCorsMappings(CorsRegistry registry) {
-	            registry.addMapping("/**").allowedOrigins("https://maisqueespecialgames.herokuapp.com");
-	        }
-	    };
-	}
+	
 }
