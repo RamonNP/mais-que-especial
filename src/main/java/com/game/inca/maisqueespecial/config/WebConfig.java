3fa6b81c6106b32eee4bd8be3a6f6c6fe976c288
Ramon do Nascimento Pereira <ramonnascimento@brq.com>
2019-07-18 19:57:26 -0300

    adicionado serviço de retorno dos jogos por login

--- src/main/java/com/game/inca/maisqueespecial/config/WebConfig.java
+++ src/main/java/com/game/inca/maisqueespecial/config/WebConfig.java
@@ -1,12 +1,24 @@
 package com.game.inca.maisqueespecial.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+@Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
 	 @Override
 	    public void addCorsMappings(CorsRegistry registry) {
-	        registry.addMapping("/**").allowedOrigins("*");
+	        registry.addMapping("/**")
+	        .allowedOrigins("*")
+	        .allowedHeaders("*")
+	        .allowedMethods("PUT")
+	        .allowedMethods("GET")
+	        .allowedMethods("POST")
+	        .allowedMethods("GET")
+	        ;
 	    }
 }
