a546c2a674d2db3aa83558f3238cd5aea702f5a2
Ramon do Nascimento Pereira <ramonnascimento@brq.com>
2019-07-18 21:16:03 -0300

    adicionado serviço de retorno dos jogos por login

--- src/main/java/com/game/inca/maisqueespecial/config/WebConfig.java
+++ src/main/java/com/game/inca/maisqueespecial/config/WebConfig.java
@@ -1,26 +1,36 @@
 package com.game.inca.maisqueespecial.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-
-	 @Override
-	    public void addCorsMappings(CorsRegistry registry) {
-	        String headers;
-			registry.addMapping("/**")
-			//.allowedHeaders(headers)
-	        .allowedOrigins("*")
-	        .allowedOrigins("*")
-	        .allowedHeaders("https://maisqueespecialgames.herokuapp.com/")
-	        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
-	        .allowCredentials(false)
-	        .maxAge(3600)
-	        ;
+	 @Bean
+	    public WebMvcConfigurer corsConfigurer() {
+	        return new WebMvcConfigurerAdapter() {
+	            @Override
+	            public void addCorsMappings(CorsRegistry registry) {
+	                registry.addMapping("/**")
+	                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS");
+	            }
+	        };
 	    }
+
+//	 @Override
+//	    public void addCorsMappings(CorsRegistry registry) {
+//	        String headers;
+//			registry.addMapping("/**")
+//			//.allowedHeaders(headers)
+////	        .allowedOrigins("*")
+////	        .allowedOrigins("*")
+////	        .allowedHeaders("https://maisqueespecialgames.herokuapp.com/")
+//	        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+//	        .allowCredentials(false)
+//	        .maxAge(3600)
+//	        ;
+//	    }
 }
